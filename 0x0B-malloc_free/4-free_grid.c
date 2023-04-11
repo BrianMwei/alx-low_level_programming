a991c1f81a6f7b8f8d9aa16e4cbfcdb3310bfb27
BrianMwei <brian.kiprono@outlook.com>
2023-04-11 11:27:02 -0700

    It's not bragging if you can back it up

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+
+ 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * free_grid - clears memory in a 2D array
+ * @grid: 2D array
+ * @height: height of the array
+ * Return: o
+ *
+ */
+void free_grid(int **grid, int height)
+{
+	int content;
+
+	for (content = 0; content < height; content++)
+	{
+		free(grid[content]);
+	}
+	free(grid);
+}
